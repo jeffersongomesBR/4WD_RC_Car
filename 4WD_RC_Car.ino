e026316d7e5b76a2a3d8c234d272056a6b49a1fb
jeffersongomesBR <jeffersongomes018@gmail.com>
2021-09-17 15:47:52 -0300

    Initial commit

---
+++ 4WD_RC_Car.ino
@@ -0,0 +1,53 @@
+  //////////////////////////
+ //    <Configuração>    //
+//////////////////////////
+
+/// ---Pinos--- ///
+
+//Motores Dianteiros
+#define FrontEgineL
+#define FrontEngineR
+#define FrontEngineV //PWM~
+
+//Motores Traseiros
+#define RearEngineL
+#define RearEngineR
+#define RearEngineV //PWM~
+
+  //////////////////
+ //   <Codigo>   //
+//////////////////
+
+//Frente - ré
+void FowardBackward() {
+  
+  //...
+}
+
+//Esquerda - Direita
+void LeftRight() {
+
+  //...
+}
+
+//Freio Traseiro
+void RearBreak() {
+
+  //...
+}
+
+//Freio Dianteiro
+void FrontBreak() {
+
+  //...
+}
+
+void setup() {
+  
+  //...
+}
+
+void loop() {
+  
+  //...
+}
