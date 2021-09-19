d9d9fc3a320f8db9702133434c6e3a9e6c2da889
JeffG <jeffersongomes018@gmail.com>
2021-09-19 04:16:45 -0300

    Update 4WD_RC_Car.ino

--- 4WD_RC_Car.ino
+++ 4WD_RC_Car.ino
@@ -1,53 +1,80 @@
 /// ---Pinos--- ///
 
 //Motores Dianteiros
-#define FrontEngineL 4
-#define FrontEngineR 5
-#define FrontEngineV 3 //PWM~
+#define FrontEngineL 4 //Unused
+#define FrontEngineR 5 //Unused
+#define FrontEngineV 3 //PWM~ //Unused
 
 //Motores Traseiros
-#define RearEngineL 7
-#define RearEngineR 8
-#define RearEngineV 6 //PWM~
+#define RearEngineL 7 //Unused
+#define RearEngineR 8 //Unused
+#define RearEngineV 6 //PWM~ //Unused
 
 //Outros
 #define FirstPin 3
 #define LastPin 7
 
+//TODO: need tests with bluetooth module
 /// ---Controles--- ///
-#define Foward w
-#define Backward s
-#define Left a
-#define Right d
-#define Break S
-#define HandBreak x
-#define Speed W
-#define Light l
-#define Reset r
+#define Foward w //Unused
+#define Backward s //Unused
+#define Left a //Unused
+#define Right d //Unused
+#define Break S //Unused
+#define HandBreak x //Unused
+#define Speed W //Unused
+#define Light l //Unused
+#define Reset r //Unused
 
 /// ---Definições--- ///
-const int deltaV = 1;
+const int deltaV = 2;
 const int lowSpeed = 64; //Minimo 0
 const int mediumSpeed = 128;
 const int highSpeed = 255; //Maximo 255
 
 /// ---Variaveis--- ///
-int @speed = 0; //0-255
-int targetSpeed = 0; //0-3
-bool lightON = false;
-bool handBreakUP = false;
+int velocity = 0; //0-255
+int targetSpeed = 0; //0-255
+int speedMode = 2; //1-3
+int moveDirection = 0 //-1 = reverse, 0 = stopped, 1 = forward //Unused
+int turnDirection = 0 //-1 = Left, 0 = stopped, 1 = Right //Unused
+bool lightON = false; //Unused
+bool handBreakUP = false; //Unused
 char key;
 
 /// ---Codigo--- ///
 
+//Velocidade
+void UpdateSpeed() {
+
+  if(velocity < targetSpeed) {
+
+    velocity += deltaV;
+  }
+  else if (velocity > targetSpeed) {
+
+    velocity -= deltaV;
+  }
+
+  //Get Difference between values
+  int speedDifference = targetSpeed - velocity;
+  speedDifference = abs(speedDifference);
+
+  //Round speed
+  if(speedDifference < deltaV) {
+
+    velocity = targetSpeed;
+  }
+}
+
 //Frente - ré
-void FowardBackward() {
+void FowardBackward(int d) {
   
   //...
 }
 
 //Esquerda - Direita
-void LeftRight() {
+void LeftRight(int d) {
 
   //...
 }
@@ -70,12 +97,6 @@ void ToggleLight() {
   //...
 }
 
-//Velocidade
-void UpdateSpeed() {
-
-  //...
-}
-
 //Reset
 void Reset() {
 
@@ -93,14 +114,37 @@ void setup() {
 }
 
 void loop() {
-  
+
+  //TODO: does buffer clear after read?
   if(Serial.available() > 0) {
 
     key = Serial.read();
   }
 
-  if(key == w) {
+  //Update Target Speed
+  switch(speedMode) {
 
-    FowardBackward();
+    case 1:
+    targetSpeed = lowSpeed;
+    break;
+
+    case 2:
+    targetSpeed = mediumSpeed;
+    break;
+
+    case 3:
+    targetSpeed = highSpeed;
+    break;
+
+    default:
+    targetSpeed = mediumSpeed;
+    break;
+  }
+
+  if(moveDirection != 0 || turnDirection != 0) {
+
+    UpdateSpeed();
+    FowardBackward(moveDirection);
+    LeftRight(turnDirection);
   }
 }
